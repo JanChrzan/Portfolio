88fc3af1007c3e2100f78414275e0ac4902f0016
JanChrzan <89533099+JanChrzan@users.noreply.github.com>
2023-02-15 23:28:04 +0100

    Reorganized code structure to enhance application scalability and reliability.

--- src/tests/FormModal.test.tsx
+++ src/tests/FormModal.test.tsx
@@ -4,10 +4,16 @@ import "@testing-library/jest-dom/extend-expect";
 import FormModal from "../components/minor/FormModal";
 
 describe("FormModal", () => {
+  const setIsModalOpen = jest.fn();
+
   it("Render title as props", () => {
     const title = "Modal Title";
     const { getByText } = render(
-      <FormModal title={title} message="Modal message" />
+      <FormModal
+        title={title}
+        message="Modal message"
+        setIsModalOpen={setIsModalOpen}
+      />
     );
     expect(getByText(title)).toBeInTheDocument();
   });
@@ -15,35 +21,46 @@ describe("FormModal", () => {
   it("Render message as props", () => {
     const message = "Modal message";
     const { getByText } = render(
-      <FormModal title="Modal Title" message={message} />
+      <FormModal
+        title="Modal Title"
+        message={message}
+        setIsModalOpen={setIsModalOpen}
+      />
     );
     expect(getByText(message)).toBeInTheDocument();
   });
 
   it("Render button with correct text", () => {
     const { getByText } = render(
-      <FormModal title="Modal Title" message="Modal message" />
+      <FormModal
+        title="Modal Title"
+        message="Modal message"
+        setIsModalOpen={setIsModalOpen}
+      />
     );
     expect(getByText("modalButton")).toBeInTheDocument();
   });
 
   it("Call handleClose function on button click", () => {
-    const handleClose = jest.fn();
     const { getByText } = render(
       <FormModal
         title="Modal Title"
         message="Modal message"
-        handleClose={handleClose}
+        setIsModalOpen={setIsModalOpen}
       />
     );
     fireEvent.click(getByText("modalButton"));
-    expect(handleClose).toHaveBeenCalled();
+    expect(setIsModalOpen).toHaveBeenCalled();
   });
 
   it("Button not call handleClose function", () => {
     const handleClose = jest.fn();
     const { getByText } = render(
-      <FormModal title="Modal Title" message="Modal message" />
+      <FormModal
+        title="Modal Title"
+        message="Modal message"
+        setIsModalOpen={setIsModalOpen}
+      />
     );
     fireEvent.click(getByText("modalButton"));
     expect(handleClose).not.toHaveBeenCalled();
